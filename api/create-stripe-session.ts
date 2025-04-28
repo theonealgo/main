fb4e8c9c670ef4c94818ca79e9ab9477221bd0ad
theonealgo <nmesghali@gmail.com>
2025-04-28 14:20:32 -0400

    Update create-stripe-session.ts

--- api/create-stripe-session.ts
+++ api/create-stripe-session.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2024-03-14' as any,
 });
 export async function POST(req: NextRequest) {
   const { plan, billing, email } = await req.json();
