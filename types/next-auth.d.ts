bcdfa8f98e7445845ea8925eebae33ec0cc1b64f
theonealgo <nmesghali@gmail.com>
2025-05-03 12:48:03 -0400

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+    user: {
+      id?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+    id?: string;
+  }
+}
