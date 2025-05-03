66de5e4f339a8f90560bf64e3fa8809cb39ab4e9
theonealgo <nmesghali@gmail.com>
2025-05-03 12:20:11 -0400

    Update [...nextauth].ts

--- api/auth/[...nextauth].ts
+++ api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -9,4 +9,27 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/signin', // Optional: Customize sign-in page
+    error: '/auth/error', // Error page
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile?.sub;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+      return session;
+    },
+  },
 });
+
+export { handler as GET, handler as POST };
