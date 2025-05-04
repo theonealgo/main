4268e2960fbb714758be1a2a534d191a868bf4b7
theonealgo <nmesghali@gmail.com>
2025-05-03 20:44:44 -0400

    Update [...nextauth].ts

--- api/auth/[...nextauth].ts
+++ api/auth/[...nextauth].ts
@@ -1,18 +1,18 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: '646378186413-hdb2ul2tdd435p4emonq68m0o9ec4c5d.apps.googleusercontent.com',
+      clientSecret: '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',
     }),
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
         const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/login`, {
@@ -29,7 +29,9 @@ export const authOptions = {
       },
     }),
   ],
-  session: { strategy: 'jwt' },
+  session: {
+    strategy: 'jwt' as const, // <- FIXED!
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
