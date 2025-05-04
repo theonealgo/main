f943e105e2ef93907a562378c092413fd1932736
theonealgo <nmesghali@gmail.com>
2025-05-04 13:06:07 -0400

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,19 +1,23 @@
 import NextAuth, { DefaultSession } from "next-auth";
-import { JWT } from "next-auth/jwt";
+import { JWT as DefaultJWT } from "next-auth/jwt";
 
 // Extend the default NextAuth session type
 declare module "next-auth" {
   interface Session extends DefaultSession {
     accessToken?: string;
     user: {
-      id?: string;
+      id: string; // Stronger typing for guaranteed use
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
   }
 }
 
 // Extend the JWT type to include the accessToken and user ID
 declare module "next-auth/jwt" {
-  interface JWT {
+  interface JWT extends DefaultJWT {
     accessToken?: string;
     id?: string;
   }
