cb8c1c6ef3929337163ede1e9f2543dc9493b84a
theonealgo <nmesghali@gmail.com>
2025-05-07 11:05:46 -0400

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,6 @@
 // app/auth/page.tsx
 'use client';
+export const dynamic = 'force-dynamic';
 
 import { useState, FormEvent } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
