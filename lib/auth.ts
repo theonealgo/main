ff9109ba21f0f0ad1b169793621132f47bab97a8
theonealgo <nmesghali@gmail.com>
2025-05-08 15:15:16 -0400

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 // lib/auth.ts
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { SupabaseAdapter } from "@next-auth/supabase-adapter";
 
@@ -9,6 +10,10 @@ export const authOptions = {
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
   }),
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
@@ -16,14 +21,11 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const res = await fetch(
-          `${process.env.NEXTAUTH_URL}/api/auth/signup`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
-          }
-        );
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/signup`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
         const user = await res.json();
         return res.ok && user ? user : null;
       },
