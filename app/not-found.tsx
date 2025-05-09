848777b796cf00cccc5b4f23a9f72f03933e00de
theonealgo <nmesghali@gmail.com>
2025-05-09 06:54:42 -0400

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-teal-400">404 — Page Not Found</h1>
+    </div>
+  );
+}
