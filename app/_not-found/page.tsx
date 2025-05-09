e281bc63d98b893b4e1815abcc79342808c0457c
theonealgo <nmesghali@gmail.com>
2025-05-08 20:41:59 -0400

    Update page.tsx

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -4,5 +4,5 @@ export default function NotFoundPage() {
     <div className="h-screen flex items-center justify-center bg-black text-white">
       <h1 className="text-4xl font-bold">404 — Page Not Found</h1>
     </div>
-  )
+  );
 }
